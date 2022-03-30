146bc141afa37b59d5a538c5b0d81081f6aa9b33
IvanLugovskiy <wotlegionb@gmail.com>
2022-03-30 18:12:23 +0300

    task01/1

--- lesson_01.js
+++ lesson_01.js
@@ -1,10 +1,24 @@
-const myFn = (x, y) => {
-  let myArr = [];
-  for (let res = x; res <= y; res++) {
-    myArr.push(res);
+const FuncReturnArr = (x, y) => {
+  let resultArr = [];
+  let i = x;
+  if (x < y) {
+    for (let i = x; i <= y; i++) resultArr.push(i);
   }
-  return myArr;
+  if (x > y) {
+    for (i = x; i >= y; i--) resultArr.push(i);
+  }
+  if (x == y) {
+    resultArr.push(i);
+  }
+  return resultArr;
 };
-console.log(myFn(5, 12));
+
+console.log(FuncReturnArr(1, 5));
+console.log(FuncReturnArr(5, 1));
+console.log(FuncReturnArr(5, 5));
 
 //F(x, y) => [x … y] принимает два числа и возвращает массив со значениями от первого до второго
+
+/* 
+нейминг - myFn, myArr - привыкай сразу такого не делать, имена должны иметь смысл, позже отдельно обсудим
+1) обработай случай, когда параметры равны или второй меньше первого */
