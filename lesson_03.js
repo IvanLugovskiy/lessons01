4e865a927ffebeaef19d059dbe416f234c860d19
IvanLugovskiy <wotlegionb@gmail.com>
2022-03-30 18:14:20 +0300

    task03/1

--- lesson_03.js
+++ lesson_03.js
@@ -1,18 +1,18 @@
-const canbePalindrome = (str) => {
-  let orign = str;
-  let arr = [];
-  arr = str.split("").reverse().join("");
-  if (arr === orign) {
-    console.log(`string - ${orign} is Palindrome`);
-  } else {
-    console.log(`string - ${orign} is not Palindrome`);
-  }
-  return console.log(arr, orign);
-};
-
-canbePalindrome("abcba");
-canbePalindrome("test");
-
 //F(‘test’) => false
 //F(‘abcba’) => true
 //Является ли строка палиндромом
+
+/* нейминг - myFn, myArr - привыкай сразу такого не делать, имена должны иметь смысл, позже отдельно обсудим
+3) может быть дорого и неоптимально, но формально работает:) попробуй работать со строкой как с массивом */
+
+function palindrome(str) {
+  let len = str.length;
+  for (let i = 0; i < len / 2; i++)
+    if (str[i] != str[len - i - 1]) {
+      return false;
+    }
+  return true;
+}
+console.log(palindrome("abcba")); // true
+console.log(palindrome("test")); // false
+console.log(palindrome("work")); // false
