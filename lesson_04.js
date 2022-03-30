f0164268dff114e268428422a10ebf29d15619d1
IvanLugovskiy <wotlegionb@gmail.com>
2022-03-30 18:16:14 +0300

    task04/1

--- lesson_04.js
+++ lesson_04.js
@@ -1,14 +1,13 @@
-const myFn = (x) => {
+const FuncReturnLogic = (x) => {
   return (y) => {
-    if (y > x) {
-      return y;
-    } else {
-      return false;
-    }
+    return y > x ? true : false;
   };
 };
-let args = myFn(10)(20);
+let args = FuncReturnLogic(30)(20);
 console.log(args);
 
 /* F(x) => ff(y) => y > x
 Вот тут забавно. Ф принимает число X, возвращает функцию, которая тоже принимает число Y и возвращает значение Y > X  */
+
+/* нейминг - FuncReturnlogic, myArr - привыкай сразу такого не делать, имена должны иметь смысл, позже отдельно обсудим
+4) возвращаться должен boolean; и попробуй обойтись без ифов, они не приносят там пользы */
