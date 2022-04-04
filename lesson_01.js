7d131a558cfb4c06bbfdf89a59859d64bb0464fe
IvanLugovskiy <wotlegionb@gmail.com>
2022-04-04 17:10:15 +0300

    task01/2

--- lesson_01.js
+++ lesson_01.js
@@ -1,24 +1,36 @@
-const FuncReturnArr = (x, y) => {
-  let resultArr = [];
-  let i = x;
+const funcReturnArrNum = (x, y) => {
+  const resultArrNum = [];
+  const subx = x;
+  const suby = y;
+
   if (x < y) {
-    for (let i = x; i <= y; i++) resultArr.push(i);
+  } else {
+    x = y;
+    y = subx;
   }
-  if (x > y) {
-    for (i = x; i >= y; i--) resultArr.push(i);
+  for (let i = x; i <= y; i++) resultArrNum.push(i);
+  if (subx > suby) {
+    resultArrNum.reverse();
   }
-  if (x == y) {
-    resultArr.push(i);
-  }
-  return resultArr;
+  return resultArrNum;
 };
 
-console.log(FuncReturnArr(1, 5));
-console.log(FuncReturnArr(5, 1));
-console.log(FuncReturnArr(5, 5));
+console.log(funcReturnArrNum(1, 5));
+console.log(funcReturnArrNum(5, 1));
+console.log(funcReturnArrNum(5, 5));
+console.log(funcReturnArrNum(10, 5));
+console.log(funcReturnArrNum(5, 10));
 
 //F(x, y) => [x … y] принимает два числа и возвращает массив со значениями от первого до второго
 
 /* 
 нейминг - myFn, myArr - привыкай сразу такого не делать, имена должны иметь смысл, позже отдельно обсудим
 1) обработай случай, когда параметры равны или второй меньше первого */
+
+/* много лишнего наворотил, два цикла одинаковые, некрасиво, можно проще
+
+если решил всегда идти от большего к меньшему - посмотри, как избавиться от одного цикла
+
+Если коротко - тебя интересует только направление от х до у
+
+Это важная штука вообще: если куски кода выглядят одинаковыми - что-то пошло не так, поленился найти хорошее решение */
