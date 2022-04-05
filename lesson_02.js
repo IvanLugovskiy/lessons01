cd228a55c73d430aa311ce559bf637ba29ce136f
IvanLugovskiy <wotlegionb@gmail.com>
2022-04-05 14:53:00 +0300

    task02/2

--- lesson_02.js
+++ lesson_02.js
@@ -1,28 +1,34 @@
-function ResultRepeet(x) {
-  //x = x.sort();
-  let len = x.length;
-  let count = 1;
-  let tmpcount = 0;
-  let showelem;
-
-  for (i = 0; i < len; i++) {
-    for (m = 0; m < len; m++) {
-      if (x[i] === x[m]) {
-        tmpcount++;
-      }
-      if (count < tmpcount) {
-        count = tmpcount;
-        showelem = x[i];
-      }
+function repeatNumber(numArr) {
+  let resultcountnum = numArr.reduce(function (Objectresult, value) {
+    if ((Objectresult[value] = Objectresult[value])) {
+      Objectresult[value]++;
+    } else {
+      Objectresult[value] = 1;
     }
-    tmpcount = 0;
-  }
-  console.log(count, showelem);
-  return count, showelem;
+    return Objectresult;
+  }, {});
+  console.log(resultcountnum);
+  let objectinarray = Object.entries(resultcountnum);
+  console.log(objectinarray);
+  sortedobjectarray = objectinarray.sort((a, b) => b[1] - a[1]);
+  sortedobjectarray.length = 1;
+  const finalcountvalue = sortedobjectarray[0];
+  const finalvalue = finalcountvalue[0];
+  const finalcount = finalcountvalue[1];
+  console.log(`the most frequent value is ${finalvalue} count ${finalcount}`);
+  return finalcountvalue;
 }
-exArray = [1, 2, 1, 3, 1, 4];
-exArray1 = [1, 2, 1, 3, 1, 4, 3, 1, 3, 3, 3, 5];
-ResultRepeet(exArray);
-//ResultRepeet(exArray1);
+
+numArr = [1, 2, 1, 3, 1, 4, 3, 4, 5];
+
+repeatNumber(numArr);
 
 //F([1,2,1,3,1,4]) => {count:3, value:1}наибольшее количество повторений числа в массиве и само число
+
+/* Нейминг - функции с маленькой буквы, используем строго camelCase,
+имя самой функции желательно делать глаголом. Представь, что это команда, которую ты даёшь, она должна читаться
+
+Задача максимум - решить без второго цикла:)
+
+Задача минимум - уменьшить число лишних операций. Для массива из 6 элементов
+ты делаешь 36 сравнений, это n^2, а можно за n!, факториал, то бишь */
